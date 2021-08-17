cc7ad5366852909fc2760206ab93d156272d1fce
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-08-17 20:36:04 +0100

    Task class is in build.gradle.kts (and it works)

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "me.hives"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.register<HelloTask>("sayHello")
+
+abstract class HelloTask : DefaultTask() {
+    @TaskAction
+    fun sayHello() {
+        println("hello from the hello task")
+    }
+}
